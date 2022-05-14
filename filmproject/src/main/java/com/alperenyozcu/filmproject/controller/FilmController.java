623f436047951fbdd07ce50d5f80feccf6437b32
alperenyozcu <79807548+alperenyozcu@users.noreply.github.com>
2022-05-15 00:22:09 +0300

    Movie Add and show

--- filmproject/src/main/java/com/alperenyozcu/filmproject/controller/FilmController.java
+++ filmproject/src/main/java/com/alperenyozcu/filmproject/controller/FilmController.java
@@ -1,21 +1,25 @@
 package com.alperenyozcu.filmproject.controller;
 
 import com.alperenyozcu.filmproject.model.Actors;
+import com.alperenyozcu.filmproject.model.Film;
 import com.alperenyozcu.filmproject.model.Languages;
-import com.alperenyozcu.filmproject.model.User;
 import com.alperenyozcu.filmproject.repository.ActorRepository;
+import com.alperenyozcu.filmproject.repository.FilmRepository;
 import com.alperenyozcu.filmproject.repository.LanguageRepository;
 import com.alperenyozcu.filmproject.service.FilmService;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.alperenyozcu.filmproject.model.Film;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/Film",method = RequestMethod.GET)
@@ -26,6 +30,8 @@ public class FilmController {
     ActorRepository actorRepository;
     @Autowired
     LanguageRepository languageRepository;
+    @Autowired
+    FilmRepository filmRepository;
 
     @GetMapping({"/AddFilms"})
     public ModelAndView home1(Model model) {
@@ -42,6 +48,13 @@ public class FilmController {
     public ModelAndView home2(Model model) {
 
         return new ModelAndView("AddActors");
+
+    }
+    @GetMapping({"/Films"})
+    public ModelAndView home5(Model model) {
+        List<Film> all = filmRepository.findAll();
+        model.addAttribute("Film",all);
+        return new ModelAndView("AdminFilms");
 
     }
     @GetMapping({"/AddLang"})
@@ -64,10 +77,23 @@ public class FilmController {
     }
 
     @PostMapping("/Add")// film kayıt
-    public String addfilm(@ModelAttribute Film film)
+    public  RedirectView addfilm(@RequestParam String filmname, @RequestParam String production, @RequestParam String type,
+                                        @RequestParam String actors, @RequestParam String languages, @RequestParam Date relased, @RequestParam String description)
     {
-        filmService.createFilm(film);
-        return "/admin/homepage";
+        Film film=new Film();
+        film.setFilmname(filmname);
+        film.setProduction(production);
+        film.setType(type);
+        film.setRelased(relased);
+        film.setDescription(description);
+        String[] strActorId = actors.split(",");
+        String[] strLangId = languages.split(",");
+        Boolean success = filmService.createFilm(film,strActorId,strLangId);
+        Gson gson = new Gson();
+        Map<String, Object> map = new HashMap<>();
+        map.put("success", success);
+
+        return new RedirectView("/Film/AddFilms");
 
     }
 }
--- filmproject/src/main/java/com/alperenyozcu/filmproject/service/FilmService.java
+++ filmproject/src/main/java/com/alperenyozcu/filmproject/service/FilmService.java
@@ -10,6 +10,9 @@ import com.alperenyozcu.filmproject.repository.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class FilmService {
 
@@ -20,9 +23,7 @@ public class FilmService {
     @Autowired
     LanguageRepository languageRepository;
 
-    public void createFilm(Film film) {
-        filmRepository.save(film);
-    }
+
 
     public void createAct(Actors actors) {
        actorRepository.save(actors);
@@ -30,5 +31,29 @@ public class FilmService {
 
     public void createLang(Languages languages) {
         languageRepository.save(languages);
+    }
+
+    public Boolean createFilm(Film film, String[] strActorId, String[] strLangId) {
+
+        try{
+
+            List<Actors> actors=new ArrayList<>();
+            List<Languages> languages=new ArrayList<>();
+            for (String actorId:strActorId) {
+             actors.add(actorRepository.findById(Integer.parseInt(actorId)).get());
+            }
+            for (String langId:strLangId) {
+                languages.add(languageRepository.findById(Integer.parseInt(langId)).get());
+            }
+            film.setActors(actors);
+            film.setLanguages(languages);
+            filmRepository.save(film);
+            return  true;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            return  false;
+        }
     }
 }
